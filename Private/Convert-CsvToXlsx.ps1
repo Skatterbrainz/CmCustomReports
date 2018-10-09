ecbcaa7c55dd02a5bf7741dd508d464a5e010be3
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-10-09 18:23:02 -0500

    Update Convert-CsvToXlsx.ps1

--- Private/Convert-CsvToXlsx.ps1
+++ Private/Convert-CsvToXlsx.ps1
@@ -1,41 +1,46 @@
-[CmdletBinding()]
-param (
-  [parameter(Mandatory=$True)]
-  $csvfiles
-)
-# $csvfiles = Get-ChildItem -path "i:\scripts\reports" -Filter "*.csv"
-
-foreach ($csvfile in $csvfiles) {
-    $csv  = $csvfile.FullName
-    $xlsx = $csv -replace ".csv",".xlsx"
-    Write-Verbose "reading: $csv"
-
-    $delimiter = "," #Specify the delimiter used in the file
-
-    # Create a new Excel workbook with one empty sheet
-    $excel = New-Object -ComObject excel.application 
-    $workbook = $excel.Workbooks.Add(1)
-    $worksheet = $workbook.Worksheets.Item(1)
-
-    # Build the QueryTables.Add command and reformat the data
-    $TxtConnector = ("TEXT;" + $csv)
-    $Connector = $worksheet.QueryTables.add($TxtConnector,$worksheet.Range("A1"))
-    $query = $worksheet.QueryTables.item($Connector.name)
+function ConvertTo-Excel {
+    param (
+        [parameter(Mandatory=$True)]
+            [ValidateNotNullOrEmpty()]
+            [string] $CsvFile,
+        [parameter(Mandatory=$True)]
+            [ValidateNotNullOrEmpty()]
+            [string] $XlFile,
+        [parameter(Mandatory=$False)]
+            [string] $delimiter = ','
+    )
+    if (!(Test-Path $CsvFile)) {
+        Write-Error "$CsvFile not found!"
+        break
+    }
+    Write-Verbose "opening an instance of microsoft excel"
+    $excel = New-Object -ComObject Excel.Application 
+    Write-Verbose "adding workbook"
+    $workbook  = $excel.Workbooks.Add(1)
+    Write-Verbose "selectincg worksheet 1"
+    $worksheet = $workbook.worksheets.Item(1)
+    $TxtConnector = ("TEXT;" + $CsvFile)
+    Write-Verbose "connector: $TxtConnector"
+    $Connector = $worksheet.QueryTables.Add($TxtConnector,$worksheet.Range("A1"))
+    $query = $worksheet.QueryTables.Item($Connector.name)
     $query.TextFileOtherDelimiter = $delimiter
     $query.TextFileParseType  = 1
     $query.TextFileColumnDataTypes = ,1 * $worksheet.Cells.Columns.Count
     $query.AdjustColumnWidth = 1
-
-    # Execute & delete the import query
     $query.Refresh()
     $query.Delete()
-
+    Write-Verbose "saving content to $XlFile"
     try {
-      $Workbook.SaveAs($xlsx,51) | Out-Null
-      Write-Verbose "saving: $xlsx"
+        $Workbook.SaveAs($XlFile,51) | Out-Null
+        Write-Verbose "saved output successfully"
+        $result = 0
     }
     catch {
-      Write-Error $_.Exception.Message
+        Write-Verbose "error: $($_.Exception.Message)"
+        $result = -1
     }
-    $excel.Quit()
+    finally {
+        $excel.Quit()
+    }
+    Write-Output $result
 }
