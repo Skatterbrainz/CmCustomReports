149d06dd52821e3973cd4214351d1a5cf5f9f373
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-10-09 15:26:02 -0500

    Update Clients-Inventory-General.sql

--- Queries/Clients-Inventory-General.sql
+++ Queries/Clients-Inventory-General.sql
@@ -1,28 +1,34 @@
 SELECT DISTINCT 
-	dbo.vWorkstationStatus.Name, 
-	dbo.vWorkstationStatus.UserName, 
+    dbo.vWorkstationStatus.Name, 
+	dbo.vWorkstationStatus.UserName,
 	dbo.v_GS_OPERATING_SYSTEM.Caption0 AS OperatingSystem, 
-	CASE
-		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 10586) THEN '1511'
-		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 14393) THEN '1607'
-		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 15063) THEN '1703'
-		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 16299) THEN '1709'
-		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 17134) THEN '1803'
-		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 7601) THEN 'SP1'
-		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 9600) THEN 'RTM'
-		ELSE dbo.v_GS_OPERATING_SYSTEM.BuildNumber0
-		END AS OsBuild, 
-	dbo.vWorkstationStatus.SystemType, 
+    CASE 
+		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 10586) THEN '1511' 
+		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 14393) THEN '1607' 
+		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 15063) THEN '1703' 
+		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 16299) THEN '1709' 
+		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 17134) THEN '1803' 
+		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 7601) THEN 'SP1' 
+		WHEN (dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 = 9600) THEN 'RTM' 
+		ELSE dbo.v_GS_OPERATING_SYSTEM.BuildNumber0 END AS OsBuild, 
+    dbo.vWorkstationStatus.SystemType, 
 	dbo.vWorkstationStatus.ClientVersion, 
 	dbo.vWorkstationStatus.UserDomain, 
-	CASE
-		WHEN (dbo.vWorkstationStatus.IsVirtualMachine = 1) THEN 'Y'
+	dbo.v_R_System.AD_Site_Name0 AS ADSite, 
+    dbo.v_GS_COMPUTER_SYSTEM.Model0 AS Model, 
+	dbo.v_GS_SYSTEM_ENCLOSURE.SerialNumber0 AS SerialNumber, 
+    CASE 
+		WHEN (dbo.vWorkstationStatus.IsVirtualMachine = 1) THEN 'Y' 
 		ELSE 'N' END AS IsVM, 
-	dbo.vWorkstationStatus.LastHealthEvaluationResult AS LastHealthEval, 
+    dbo.vWorkstationStatus.LastHealthEvaluationResult AS LastHealthEval, 
 	dbo.vWorkstationStatus.LastHardwareScan AS LastHwScan, 
-	dbo.vWorkstationStatus.LastDDR
+	DATEDIFF(dd,dbo.vWorkstationStatus.LastHardwareScan,GETDATE()) AS InvAge,
+    dbo.vWorkstationStatus.LastDDR
 FROM 
-	dbo.vWorkstationStatus LEFT OUTER JOIN
-	dbo.v_GS_OPERATING_SYSTEM ON dbo.vWorkstationStatus.ResourceID = dbo.v_GS_OPERATING_SYSTEM.ResourceID
-ORDER BY
-	Name
+	dbo.vWorkstationStatus INNER JOIN
+    dbo.v_R_System ON dbo.vWorkstationStatus.ResourceID = dbo.v_R_System.ResourceID LEFT OUTER JOIN
+    dbo.v_GS_SYSTEM_ENCLOSURE ON dbo.vWorkstationStatus.ResourceID = dbo.v_GS_SYSTEM_ENCLOSURE.ResourceID LEFT OUTER JOIN
+    dbo.v_GS_COMPUTER_SYSTEM ON dbo.vWorkstationStatus.ResourceID = dbo.v_GS_COMPUTER_SYSTEM.ResourceID LEFT OUTER JOIN
+    dbo.v_GS_OPERATING_SYSTEM ON dbo.vWorkstationStatus.ResourceID = dbo.v_GS_OPERATING_SYSTEM.ResourceID
+ORDER BY 
+	dbo.vWorkstationStatus.Name
