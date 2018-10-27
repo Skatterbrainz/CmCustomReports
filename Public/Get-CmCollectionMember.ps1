3ffe8250b62f5c1c8ce147bc344bb14bf49919c7
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-10-27 10:01:01 -0400

    Update Get-CmCollectionMember.ps1

--- Public/Get-CmCollectionMember.ps1
+++ Public/Get-CmCollectionMember.ps1
@@ -3,17 +3,21 @@
 .DESCRIPTION
     Get ConfigMgr Collection Members
 .PARAMETER CollectionID
-    ConfigMgr device collection ID
+    ConfigMgr Collection ID
 .PARAMETER ServerName
-    ConfigMgr SQL server host
+    ConfigMgr SQL Server hostname (FQDN)
 .PARAMETER SiteCode
     ConfigMgr Site Code
 .PARAMETER Choose
-    select collection ID from grid view query
+    Display collection names and ID numbers in grid view for selecting
 .PARAMETER Ping
-    ping each machine in the collection
+    Perform a ping test on each machine
 .PARAMETER Grid
-    display results in gridview
+    Display results in gridview
+.EXAMPLE 
+    .\Get-CmCollectionMember.ps1 -CollectionID "P010015E" -ServerName "cm01.contoso.local" -SiteCode "P01"
+.EXAMPLE
+    .\Get-CmCollectionMember.ps1 -Choose -ServerName "cm01.contoso.local" -SiteCode "P01"
 #>
 
 [CmdletBinding()]
@@ -21,12 +25,12 @@ param (
     [parameter(Mandatory=$False, ValueFromPipeline=$True, HelpMessage="Collection ID")]
         [ValidateNotNullOrEmpty()]
         [string] $CollectionID = "SMS00001",
-    [parameter(Mandatory=$False, HelpMessage="Site Database Server Name")]
+    [parameter(Mandatory=$True, HelpMessage="Site Database Server Name")]
         [ValidateNotNullOrEmpty()]
-        [string] $ServerName = "cm01.contoso.local",
-    [parameter(Mandatory=$False, HelpMessage="ConfigMgr Site Code")]
+        [string] $ServerName,
+    [parameter(Mandatory=$True, HelpMessage="ConfigMgr Site Code")]
         [ValidateNotNullOrEmpty()]
-        [string] $SiteCode = "P01",
+        [string] $SiteCode,
     [parameter(Mandatory=$False, HelpMessage="Select Collection using Gridview")]
         [switch] $Choose,
     [parameter(Mandatory=$False, HelpMessage="Query if computer is online")]
@@ -34,6 +38,7 @@ param (
     [parameter(Mandatory=$False, HelpMessage="Display results in gridview")]
         [switch] $Grid
 )
+$DatabaseName = "CM_$SiteCode"
 $QueryTimeout = 120
 $ConnectionTimeout = 30
 $conn = New-Object System.Data.SqlClient.SQLConnection
@@ -73,24 +78,24 @@ if ($Choose) {
 
 $qtext = @"
 SELECT DISTINCT 
-    dbo.v_R_System.Name0 AS Computer, 
+    dbo.v_R_System.Name0 AS ComputerName, 
     dbo.v_R_System.ResourceID, 
     dbo.v_R_System.User_Name0 AS UserName, 
     dbo.v_R_System.AD_Site_Name0 AS ADSite, 
     dbo.v_R_System.Operating_System_Name_and0 AS OS, 
     dbo.v_R_System.Build01 AS OSBuild, 
-    CASE WHEN (Client0 = 1) THEN 'Y' ELSE 'N' END AS Client, 
+    CASE WHEN (Client0 = 1) THEN 'YES' ELSE 'NO' END AS Client, 
     dbo.v_R_System.Client_Version0 AS ClientVersion, 
-    CASE WHEN (Active0 = 1) THEN 'Y' ELSE 'N' END AS Active, 
+    CASE WHEN (Active0 = 1) THEN 'YES' ELSE 'NO' END AS Active, 
     dbo.v_R_System.Distinguished_Name0, 
     dbo.v_R_System.Full_Domain_Name0 AS DNSDomain, 
-    dbo.v_R_System.Is_Virtual_Machine0 AS IsVM, 
+    dbo.v_R_System.Is_Virtual_Machine0 AS IsVirtual, 
     dbo.v_R_System.Last_Logon_Timestamp0 AS LogonTime, 
     dbo.v_R_System.User_Domain0 AS Domain 
 FROM dbo.v_R_System INNER JOIN
     dbo.v_ClientCollectionMembers ON dbo.v_R_System.ResourceID = dbo.v_ClientCollectionMembers.ResourceID
 WHERE (dbo.v_ClientCollectionMembers.CollectionID = '$CollectionID')
-ORDER BY Computer
+ORDER BY ComputerName
 "@
 
 $cmd = New-Object System.Data.SqlClient.SqlCommand($qtext,$conn)
@@ -106,34 +111,50 @@ if ($rowcount -gt 0) {
         $($ds.Tables).Rows | Out-GridView -Title "Collection Members: $CollectionID"
     }
     else {
-        $($ds.Tables).Rows |
-            Foreach-Object {
-                if ($Ping) {
-                    if (Test-NetConnection -ComputerName $_.ComputerName -InformationLevel Quiet) {
-                        $Online = $True
-                    }
-                    else {
-                        $Online = $False
-                    }
-                    $data = [ordered]@{
-                        ComputerName = $_.ComputerName
-                        ResourceID   = $_.ResourceID
-                        IsClient	 = $_.IsClient
-                        Domain       = $_.Domain
-                        SiteCode     = $_.SiteCode
-                        IsOnline     = $Online
-                    }
+        $result = $($ds.Tables).Rows
+        foreach ($row in $result) {
+            if ($Ping) {
+                if (Test-NetConnection -ComputerName $row.ComputerName -InformationLevel Quiet) {
+                    $Online = $True
                 }
                 else {
-                    $data = [ordered]@{
-                        ComputerName = $_.ComputerName
-                        ResourceID   = $_.ResourceID
-                        IsClient	 = $_.IsClient
-                        Domain       = $_.Domain
-                        SiteCode     = $_.SiteCode
-                    }
+                    $Online = $False
                 }
-                New-Object PSObject -Property $data
-            } # foreach-object
+                $data = [ordered]@{
+                    ComputerName  = $row.ComputerName
+                    ResourceID    = $row.ResourceID
+                    UserName      = $row.UserName
+                    ADSiteName    = $row.ADSite
+                    OSName        = $row.OS
+                    OSBuild       = $row.OSBuild
+                    IsVirtual     = $row.IsVirtual
+                    LastLogon     = $row.LogonTime
+                    IsClient	  = $row.Client
+                    ClientVersion = $row.ClientVersion
+                    IsActive      = $row.Active
+                    Domain        = $row.Domain
+                    SiteCode      = $SiteCode
+                    IsOnline      = $Online
+                }
+            }
+            else {
+                $data = [ordered]@{
+                    ComputerName  = $row.ComputerName
+                    ResourceID    = $row.ResourceID
+                    UserName      = $row.UserName
+                    ADSiteName    = $row.ADSite
+                    OSName        = $row.OS
+                    OSBuild       = $row.OSBuild
+                    IsVirtual     = $row.IsVirtual
+                    LastLogon     = $row.LogonTime
+                    IsClient	  = $row.Client
+                    ClientVersion = $row.ClientVersion
+                    IsActive      = $row.Active
+                    Domain        = $row.Domain
+                    SiteCode      = $SiteCode
+                }
+            }
+            New-Object -TypeName PSObject -Property $data
+        } # foreach
     } # if-else
 }
