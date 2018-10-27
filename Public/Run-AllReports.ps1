c98b3de7914a91d6675dd2b791075622ca80e48a
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-10-27 09:30:43 -0400

    Update Run-AllReports.ps1

--- Public/Run-AllReports.ps1
+++ Public/Run-AllReports.ps1
@@ -1,7 +1,9 @@
 param (
+  [string] $SQLServerName = "cm01.contoso.local",
+  [string] $SiteCode = "P01",
   [string] $QPath = ".\queries",
   [string] $OutputPath = ".\reports"
 )
 Get-ChildItem -Path $QPath -Filter "*.sql" | 
     Select-Object -ExpandProperty Fullname |
-        ForEach-Object { .\Run-CmCustomReport.ps1 -QueryFile $_ -Output Csv -OutputPath $OutputPath -Verbose }
+        ForEach-Object { .\Run-CmCustomReport.ps1 -ServerName $SQLServerName -SiteCode $SiteCode -QueryFile $_ -Output Csv -OutputPath $OutputPath -Verbose }
